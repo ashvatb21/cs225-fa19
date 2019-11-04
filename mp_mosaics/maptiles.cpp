b8c2832e624773723e6c591e49cc442b6ef93d4f
Ashvat Bansal <ashvatb2@illinoi.edu>
2019-11-04 14:19:14 -0600

    Part 2 complete and added Partners.txt file

--- mp_mosaics/maptiles.cpp
+++ mp_mosaics/maptiles.cpp
@@ -22,6 +22,35 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
      * @todo Implement this function!
      */
 
-    return NULL;
-}
 
+    map<Point<3>, size_t> map;
+    vector<Point<3>> points;
+
+    for (size_t i = 0; i < theTiles.size(); i++) {
+
+      LUVAPixel pixel = theTiles[i].getAverageColor();
+      Point<3> point = convertToXYZ(pixel);
+      
+      points.push_back(point);
+      map.insert(std::pair<Point<3>, size_t>(point,i));
+
+    }
+
+    MosaicCanvas* Canvas = new MosaicCanvas(theSource.getRows(), theSource.getColumns());
+    KDTree<3>* kd_tree = new KDTree<3>(points);
+
+    for (int i = 0; i < theSource.getRows(); i++) {
+      for (int j = 0; j < theSource.getColumns(); j++) {
+
+        Point<3> point = convertToXYZ(theSource.getRegionColor(i, j));
+        Point<3> nearest = kd_tree->findNearestNeighbor(point);
+
+        size_t best = map[nearest];
+        Canvas->setTile(i, j, &theTiles[best]);
+
+      }
+    }
+
+    delete kd_tree; //Added delete for memory check, remove if segfault
+    return Canvas;
+}
