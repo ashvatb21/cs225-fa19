1cd2f8f4b05df6fffccb0355ea93488769344f75
Ashvat Bansal <ashvatb2@illinoi.edu>
2019-11-14 20:04:31 -0600

    Initial Working code Commit

--- mp_mazes/dsets.cpp
+++ mp_mazes/dsets.cpp
@@ -1,1 +1,23 @@
 /* Your code here! */
+
+#include "dsets.h"
+using namespace std;
+
+void DisjointSets::addelements(int num) {
+  for (int i = 1; i <= num; i++) {
+    elements.push_back(-1);
+  }
+  return;
+}
+
+int DisjointSets::find(int elem){
+  return 1;
+}
+
+void DisjointSets::setunion(int a, int b){
+
+}
+
+int DisjointSets::size(int elem){
+  return 1;
+}
