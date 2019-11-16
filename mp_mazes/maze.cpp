33b7c754f496d4737e11bf0c2353a564fc0b9684
Ashvat Bansal <ashvatb2@illinoi.edu>
2019-11-15 20:53:47 -0600

    Working code initial commit

--- mp_mazes/maze.cpp
+++ mp_mazes/maze.cpp
@@ -1,1 +1,46 @@
-/* Your code here! */
+#include "maze.h"
+#include <sys/time.h>
+#include <vector>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+SquareMaze::SquareMaze() {
+}
+
+void SquareMaze::makeMaze(int width, int height) {
+
+}
+
+bool SquareMaze::canTravel(int x, int y, int dir) const {
+  return false;
+}
+
+void SquareMaze::setWall(int x, int y, int dir, bool exists) {
+
+}
+
+
+
+vector<int> SquareMaze::solveMaze(){
+    vector <int> a;
+    return a;
+}
+
+
+
+PNG* SquareMaze::drawMaze() const{
+  PNG* canvas = new PNG();
+  return canvas;
+}
+
+
+PNG* SquareMaze::drawMazeWithSolution(){
+  PNG* canvas = new PNG();
+  return canvas;
+}
+
+PNG* SquareMaze::drawCreativeMaze(){
+  PNG* canvas = new PNG();
+  return canvas;
+}
