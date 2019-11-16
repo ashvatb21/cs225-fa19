2b56cc1010f2bc87d1d5baae6bf305928d18db1d
Ashvat Bansal <ashvatb2@illinoi.edu>
2019-11-15 22:27:20 -0600

    12 Tests passed

--- mp_mazes/maze.cpp
+++ mp_mazes/maze.cpp
@@ -5,13 +5,19 @@
 #include <iostream>
 using namespace std;
 
+//Note to Self: Convert all similar additions to similar format to reduce confusion
+
 SquareMaze::SquareMaze() {
+  height_ = 0;
+  width_ = 0;
+  dsets = DisjointSets();
+  size = 0;
 }
 
 void SquareMaze::makeMaze(int width, int height) {
   width_ = width;
   height_ = height;
-  size = width * height;
+  size = width_ * height_;
   dsets.addelements(size);
 
   for (int i = 0; i < size; i++) {
@@ -26,24 +32,42 @@ void SquareMaze::makeMaze(int width, int height) {
 
     if (rand() % 2 != 1) {
       if (y != height_ - 1) {
-        if (dsets.find(y * width_ + x) != dsets.find(y * width_ + x + width_)) {
+        if (dsets.find(x + y * width_) != dsets.find(x + y * width_ + width_)) {
           setWall(x, y, 1, false);
-            dsets.setunion(y * width_ + x, y * width_ + x + width_);
+            dsets.setunion(x + y * width_, x + y * width_ + width_);
         }
       }
-    } 
+    } else {
+      if (x != width_ - 1) {
+        if (dsets.find(x + y * width_) != dsets.find(x + y * width_ + 1)){
+          rightWalls[y * width_ + x] = false;
+          dsets.setunion(x + y * width_, x + y * width_ + 1);
+        }
+      }
+    }
   }
 }
 
 bool SquareMaze::canTravel(int x, int y, int dir) const {
-  return false;
+
+  if (dir == 0) {
+    return rightWalls[x + y * width_] == false;
+  } else if (dir == 1) {
+    return downWalls[x + y * width_] == false;
+  } else if (dir == 2) {
+    return (x != 0) && (rightWalls[x + y * width_ - 1] == false);
+  } else if (dir == 3) {
+    return (y != 0) && (downWalls[x + y * width_ - width_] == false);
+  } else {
+    return false;
+  }
 }
 
 void SquareMaze::setWall(int x, int y, int dir, bool exists) {
   if (dir == 0) {
-    rightWalls[y * width_ + x] = exists;
+    rightWalls[x + y * width_] = exists;
   } else if (dir == 1) {
-    downWalls[y * width_ + x] = exists;
+    downWalls[x + y * width_] = exists;
   }
 }
 
